441725c3a999130f6ca77c0ca85f1cacd7c6d380
JURU <jurassu10@gmail.com>
2019-02-26 10:57:26 +0200

    add the required libraries imports in the app initialization module

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
-# Initializing application
-app = Flask(__name__)
-
-from app import views
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
