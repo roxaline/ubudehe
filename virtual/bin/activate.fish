25d2e707add3bfa2e81cd67d070777e5aad791c8
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2019-03-07 16:02:01 +0200

    first commit

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wecode/Desktop/DATABASE/pitches/virtual"
+set -gx VIRTUAL_ENV "/home/wecode/Documents/ubudehe/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
